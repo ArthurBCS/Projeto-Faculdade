68b2c5453f045dfde7144c1427dfd443e0aceefc
ArthurBCS <149312481+ArthurBCS@users.noreply.github.com>
2023-11-07 17:30:42 -0300

    Site-Paradigmas

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prev: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
